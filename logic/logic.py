622f62d58de2909df32f8fced6e09ff45d702918
Ali Bahrmai <a.bahrami.ce@gmail.com>
2025-12-02 18:36:20 +0330

    Fix the bug in module.
    bug was in the win funciton of logic class

--- logic/logic.py
+++ logic/logic.py
@@ -53,17 +53,21 @@ hangman_ascii = [r'''
 
 class logic():
         
-    def win(self, guess = '', game_word = '', attempt = 6):
-        if guess in game_word:
-            return guess
-        else:
-            return hangman_ascii[attempt]
+    def win(self, guess, game_word, attempt):
+      if guess in game_word: 
+        hangman = hangman_ascii[attempt]
+        return True, hangman, attempt
+      
+      else:
+        attempt += 1
+        hangman = hangman_ascii[attempt]
+        return False, hangman, attempt
         
     def save_results(self):
-        with open('save.csv', mode='w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
+      with open('save.csv', mode='w', newline='', encoding='utf-8') as file:
+          writer = csv.writer(file)
             
             
-p = logic()
+# p = logic()
 
-print(p.win('c' ,'ali', 4))
+# print(p.win('c' ,'ali', 4))
